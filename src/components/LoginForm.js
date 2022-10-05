11179a1a48cd00d5dfdd65e0ec10491fcfe2b525
Erick Siller <ErickSillerInv@hotmail.com>
2022-10-05 17:12:52 -0500

    creados login y register, falla el onChange, cambiar por onSubmit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,79 +1,99 @@
-import React,{Component, useState} from 'react';
+import React from 'react'
+import { useState } from 'react';
+import { signInWithEmailAndPassword,onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../firebase' 
 import logo from '../assets/imgs/pressura-logotitle-white.png'
 
-export class LoginForm extends Component{
+export default function LoginForm() {
+  
+    // const [user, setUser] = useState({});
+      
+    //   onAuthStateChanged(auth, (currentUser) => {
+    //       setUser(currentUser);
+    //   });
+  
+    // const [loginEmail, setLoginEmail] = useState('')
+    // const [loginPassword, setLoginPassword] = useState('')
+  
+    // const login = async () => {
+    //     try{
+    //         const user = await signInWithEmailAndPassword(auth,loginEmail,loginPassword);
+    //         console.log(user);
+    //     }
+    //     catch (error){
+    //         console.log(error.message);
+    //     }
+        
+    // }
+    // const logout = async () => {
+    //     await signOut(auth);
+    // }
 
-    render(){
-        return (
-            <h1>Hola</h1>
-        );
-        // const [loginEmail, setLoginEmail] = useState('')
-        // const [loginPassword, setLoginPassword] = useState('')
-    
-        // const login = async () => {
-            
-        // }
-        // const logout = async () => {
-            
-        // }
+  return (
+    <div className="main">
 
-        // return (
+        <div className='imgs'>
+            <img src={logo} className='logo' ></img>
+        </div>
 
-        //     <div className="main">
+        {/* <h3>Ususario logeado: {user?.email}</h3> */}
+        
 
-        //         <div className='imgs'>
-        //             <img src={logo} className='logo' ></img>
-        //         </div>
+        <div className="sub-main">
 
-        //         <div className="sub-main">
+            <div>
 
-        //             <div>
+                <div>
+                    <div style={{paddingTop: '20px'}}>
+                        <h1>Inicia Sesion</h1>
+                    </div>
+                    
+                    <div className='login-label-1'>
+                        Correo Electronico:
+                    </div>
 
-        //                 <div>
-        //                     <div style={{paddingTop: '20px'}}>
-        //                         <h1>Inicia Sesion</h1>
-        //                     </div>
-                            
-        //                     <div className='login-label-1'>
-        //                         Correo Electronico:
-        //                     </div>
+                    <div className="second-input">
+                        <input type="text" placeholder="ejemplo@hotmail.com" className="name" 
+                        //     onChange={(event) => {
+                        //     setLoginEmail(event.target.value);
+                        // }}
+                         />
+                    </div>
+                    <div className='login-label'>
+                        Contraseña:
+                    </div>
+                    <div className="second-input">
+                        <input type="password" placeholder="Contraseña" className="name"
+                        //  onChange={(event) => {
+                        //     setLoginPassword(event.target.value);
+                        // }} 
+                        />
+                    </div>
+                    
+                    <div className='button-group'>
+                        
+                        <button className="login-button" 
+                        // onClick={login}
+                        >Login</button>
+                        {/* <button className="login-button-google">Login con google</button> */}
+                        <button  
+                        // onClick={logout} 
+                        className="login-button-google">Salir de la Cuenta</button>
+                        
+                    </div>
 
-        //                     <div className="second-input">
-        //                         <input type="text" placeholder="ejemplo@hotmail.com" className="name" onChange={(event) => {
-        //                             setLoginEmail(event.target.value);
-        //                         }} />
-        //                     </div>
-        //                     <div className='login-label'>
-        //                         Contraseña:
-        //                     </div>
-        //                     <div className="second-input">
-        //                         <input type="password" placeholder="Contraseña" className="name" onChange={(event) => {
-        //                             setLoginPassword(event.target.value);
-        //                         }} />
-        //                     </div>
-                            
-        //                     <div className='button-group'>
-                                
-        //                         <button className="login-button">Login</button>
-        //                         <button className="login-button-google">Login con google</button>
-                               
-        //                     </div>
-
-        //                     <div className="link" style={{paddingTop: '20px'}}>
-        //                         <p>
-        //                             No tienes cuenta? <a href="#">Registrate</a>
-        //                         </p>
-        //                     </div>
+                    <div className="link" style={{paddingTop: '20px'}}>
+                        <p>
+                            No tienes cuenta? <a href="#">Crea una aqui</a>
+                        </p>
+                    </div>
 
 
-        //                 </div>
-        //             </div>
+                </div>
+            </div>
 
 
-        //         </div>
-        //     </div>
-        // );
-
-    }
-
+        </div>
+    </div>
+  )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,74 +1,98 @@
-import React,{Component, useState} from 'react';
+import React from 'react'
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from '../firebase' 
 import logo from '../assets/imgs/pressura-logotitle-white.png'
 
-export class Register extends Component{
+export default function Register() {
 
-    render(){
+    const [registerEmail, setRegisterEmail] = useState('')
 
-        const [registerEmail, setRegisterEmail] = useState('')
-        const [registerPassword, setRegisterPassword] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+
+  const [user, setUser] = useState({});
     
-        const register = async () => {
-            
-        }
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
 
-        return (
+  const register = async () => {
+    try{
+      const user = await createUserWithEmailAndPassword(auth,registerEmail,registerPassword);
+      console.log(user);
+    }
+    catch (error){
+      console.log(error.message);
+    }
+  }
 
-            <div className="main">
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
 
-                <div className='imgs'>
-                    <img src={logo} className='logo' ></img>
-                </div>
+  const login = async () => {
+      
+  }
+  const logout = async () => {
+      await signOut(auth);
+  }
 
-                <div className="sub-main">
+  return (
+    <div className="main">
 
-                    <div>
+        <div className='imgs'>
+            <img src={logo} className='logo' ></img>
+        </div>
 
-                        <div>
-                            <div style={{paddingTop: '20px'}}>
-                                <h1>Inicia Sesion</h1>
-                            </div>
-                            
-                            <div className='login-label-1'>
-                                Correo Electronico:
-                            </div>
+        <h3>Ususario logeado: {user?.email}</h3>
+        
 
-                            <div className="second-input">
-                                <input type="text" placeholder="ejemplo@hotmail.com" className="name" onChange={(event) => {
-                                    setRegisterEmail(event.target.value);
-                                }} />
-                            </div>
-                            <div className='login-label'>
-                                Contraseña:
-                            </div>
-                            <div className="second-input">
-                                <input type="password" placeholder="Contraseña" className="name" onChange={(event) => {
-                                    setRegisterPassword(event.target.value);
-                                }} />
-                            </div>
-                            
-                            <div className='button-group'>
-                                
-                                <button className="login-button">Login</button>
-                                <button className="login-button-google">Login con google</button>
-                               
-                            </div>
+        <div className="sub-main">
 
-                            <div className="link" style={{paddingTop: '20px'}}>
-                                <p>
-                                    Ya tienes cuenta? <a href="#">Ingresa Aqui</a>
-                                </p>
-                            </div>
+            <div>
 
+                <div>
+                    <div style={{paddingTop: '20px'}}>
+                        <h1>Registrate</h1>
+                    </div>
+                    
+                    <div className='login-label-1'>
+                        Correo Electronico:
+                    </div>
 
-                        </div>
+                    <div className="second-input">
+                        <input type="text" placeholder="ejemplo@hotmail.com" className="name" onChange={(event) => {
+                            setRegisterEmail(event.target.value);
+                        }} />
+                    </div>
+                    <div className='login-label'>
+                        Contraseña:
+                    </div>
+                    <div className="second-input">
+                        <input type="password" placeholder="Contraseña" className="name" onChange={(event) => {
+                            setRegisterPassword(event.target.value);
+                        }} />
+                    </div>
+                    
+                    <div className='button-group'>
+                        
+                        <button className="login-button" onClick={register}>Login</button>
+                        <button className="login-button-google">Login con google</button>
+                        <button  onClick={logout} className="login-button-google">signOut</button>
+                        
+                    </div>
+
+                    <div className="link" style={{paddingTop: '20px'}}>
+                        <p>
+                            Ya tienes cuenta? <a href="#">Ingresa Aqui</a>
+                        </p>
                     </div>
 
 
                 </div>
             </div>
-        );
 
-    }
 
+        </div>
+    </div>
+  )
 }
